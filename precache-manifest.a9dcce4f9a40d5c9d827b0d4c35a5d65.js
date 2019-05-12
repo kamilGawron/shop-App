58a6fcff9e2bb287f8d175f0ed1f8cbc5d313360
Kamil Gawron <kgawron955@gmail.com>
2019-05-12 19:58:51 +0200

    Updates

---
+++ precache-manifest.a9dcce4f9a40d5c9d827b0d4c35a5d65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f709ef7ae774fd77c39f4cea616daf",
+    "url": "/shop-App/index.html"
+  },
+  {
+    "revision": "d4412f80b6c46135664b",
+    "url": "/shop-App/static/css/main.1032825e.chunk.css"
+  },
+  {
+    "revision": "34cd2dd0277f9a3b76ed",
+    "url": "/shop-App/static/js/2.aba3a867.chunk.js"
+  },
+  {
+    "revision": "d4412f80b6c46135664b",
+    "url": "/shop-App/static/js/main.ff14951d.chunk.js"
+  },
+  {
+    "revision": "d941ade9fa9d553c1dd2",
+    "url": "/shop-App/static/js/runtime~main.b87a2284.js"
+  }
+]);
